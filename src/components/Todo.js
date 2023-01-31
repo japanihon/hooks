e35dee80a8fb0ad71dda4eb8b0ff7dac3579f9e8
andrustn85 <rustandres@gmail.com>
2023-01-31 10:04:35 +0100

    hooks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,59 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
     
 import CreateTask from './CreateTask';
 
-    function Task({ task }) {
-        return (
-            <div
-                className="task"
-                style={{ textDecoration: task.completed ? "line-through" : "" }}
-            >
-                {task.title}
-            </div>
-        );
-    }
+function Task({ task, index, completeTask, removeTask }) {
+    return (
+        <div
+            className="task"
+            style={{ textDecoration: task.completed ? "line-through" : "" }}
+        >
+            {task.title}
+            <button style={{ background: "red" }} onClick={() => removeTask(index)}>x</button>
+            <button onClick={() => completeTask(index)}>Complete</button>
+        </div>
+    );
+}
 
 
-    function Todo() {
-        const [tasks, setTasks] = useState([
-            {
-                title: "Grab some Pizza",
-                completed: true
-            },
-            {
-                title: "Do your workout",
-                completed: true
-            },
-            {
-                title: "Hangout with friends",
-                completed: false
-            }
-        ]);
+function Todo() {
+    const [tasksRemaining, setTasksRemaining] = useState(0);
+    const [tasks, setTasks] = useState([
+        {
+            title: "Add Your New Task",
+            completed: false
+        },
+    ]);
+      
+    useEffect(() => { 
+      setTasksRemaining(tasks.filter(task => !task.completed).length) 
+    }, [tasks]);
 
-
-        const addTask = title => {
-            const newTasks = [...tasks, { title, completed: false }];
-            setTasks(newTasks);
-        };
-
-
-        return (
-            <div className="todo-container">
-                <div className="header">TODO - ITEMS</div>
-                <div className="tasks">
-                    {tasks.map((task, index) => (
-                        <Task
-                            task={task}
-                            index={index}
-                            key={index}
-                        />
-                    ))}
-                </div>
-                <div className="create-task" >
-                    <CreateTask addTask={addTask} />
-                </div>
-            </div>
-        );
-    }
+    const addTask = title => {
+        const newTasks = [...tasks, { title, completed: false }];
+        setTasks(newTasks);
+    };
     
+    const completeTask = index => {
+        const newTasks = [...tasks];
+        newTasks[index].completed = true;
+        setTasks(newTasks);
+    };
+    
+    const removeTask = index => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+    };
+
+    return (
+        <div className="todo-container">
+            <div className="header"> Todo List / Pending tasks ({tasksRemaining})</div>
+            <div className="tasks">
+                {tasks.map((task, index) => (
+                    <Task
+                    task={task}
+                    index={index}
+                    completeTask={completeTask}
+                    removeTask={removeTask}
+                    key={index}
+                    />
+                ))}
+            </div>
+            <div className="create-task" >
+                <CreateTask addTask={addTask} />
+            </div>
+        </div>
+    );
+}
     export default Todo;
